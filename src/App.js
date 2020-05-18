227774f9f1e20e5418cfae25f62fffdc8ac97c62
hebr3 <brook057@cougars.csusm.edu>
2020-05-18 10:07:08 -0700

    Added handleClick to start tracking cells that are selected

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import React, { useState } from "react";
-import { range } from "ramda";
+import React, { useState, useEffect } from "react";
+import * as R from "ramda";
 import shuffle from "./utils/shuffle";
 import "./App.css";
 
 function App() {
-  const cells = shuffle(range(1, 10));
+  const [clickedCells, setClickedCells] = useState([]);
+  const [cells, setCells] = useState([]);
+  const handleClick = (i) => setClickedCells(R.uniq([...clickedCells, i]));
+
+  useEffect(() => {
+    setCells(shuffle(R.range(1, 10)));
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
@@ -13,11 +19,16 @@ function App() {
       <div className="board">
         {cells &&
           cells.map((i) => (
-            <div className={i < 5 ? "cell-active" : "cell"} key={i}>
+            <div
+              className={i < 5 ? "cell-active" : "cell"}
+              key={i}
+              onClick={() => handleClick(i)}
+            >
               {i}
             </div>
           ))}
       </div>
+      <pre>{JSON.stringify(clickedCells, 0, 2)}</pre>
     </div>
   );
 }
