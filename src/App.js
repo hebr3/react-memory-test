591347b635abba5b970fbff22fac2d61b9c5aa16
hebr3 <brook057@cougars.csusm.edu>
2020-05-18 10:57:18 -0700

    Added logic to allow for different number of columns

--- src/App.js
+++ src/App.js
@@ -4,23 +4,32 @@ import shuffle from "./utils/shuffle";
 import "./App.css";
 
 function App() {
+  const [width, setWidth] = useState(4);
   const [clickedCells, setClickedCells] = useState([]);
-  const [cells, setCells] = useState([]);
+  const [cells, setCells] = useState(R.range(1, width * width + 1));
   const handleClick = (i) => setClickedCells(R.uniq([...clickedCells, i]));
 
   useEffect(() => {
-    setCells(shuffle(R.range(1, 10)));
+    setCells(shuffle(cells));
   }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <h4>Memory Test</h4>
       </header>
-      <div className="board">
+      <div
+        className="board"
+        style={{
+          gridTemplateColumns: R.range(0, width)
+            .map((i) => "1fr")
+            .join(" "),
+        }}
+      >
         {cells &&
           cells.map((i) => (
             <div
-              className={i < 5 ? "cell-active" : "cell"}
+              className={i < (width * width) / 2 ? "cell-active" : "cell"}
               key={i}
               onClick={() => handleClick(i)}
             >
