1947de53d51e290817b3b04635dfc43396dfa224
hebr3 <brook057@cougars.csusm.edu>
2020-05-17 21:25:18 -0700

    Updates

---
+++ precache-manifest.3c9895b6e2a9dd7b8cec5aeb29bba78d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f6e59f2bbc1f12b1f4139de37ee94d2",
+    "url": "/react-memory-test/index.html"
+  },
+  {
+    "revision": "22b4b2917a8c70c0645f",
+    "url": "/react-memory-test/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "58dca2088f2be23149f8",
+    "url": "/react-memory-test/static/js/2.dcbb66d7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-memory-test/static/js/2.dcbb66d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22b4b2917a8c70c0645f",
+    "url": "/react-memory-test/static/js/main.49288c1c.chunk.js"
+  },
+  {
+    "revision": "97be1efb7bf198fb0c6a",
+    "url": "/react-memory-test/static/js/runtime-main.7128a991.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-memory-test/static/media/logo.5d5d9eef.svg"
+  }
+]);
