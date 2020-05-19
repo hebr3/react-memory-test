000ed4b0cdb07022714307df5d8d8d6462e0d6fe
hebr3 <brook057@cougars.csusm.edu>
2020-05-18 22:32:04 -0700

    Switched from div to button to avoid an a11y error message

--- src/App.js
+++ src/App.js
@@ -7,11 +7,32 @@ function App() {
   const [width, setWidth] = useState(4);
   const [clickedCells, setClickedCells] = useState([]);
   const [cells, setCells] = useState(R.range(1, width * width + 1));
-  const handleClick = (i) => setClickedCells(R.uniq([...clickedCells, i]));
+  const handleClick = (i) => {
+    setClickedCells(R.uniq([...clickedCells, i]));
+  };
 
   useEffect(() => {
     setCells(shuffle(cells));
   }, []);
+
+  const Cell = (idx) => {
+    if (idx < (width * width) / 2) {
+      return (
+        <button
+          className="cell-active"
+          key={idx}
+          onClick={() => handleClick(idx)}
+        >
+          {idx}
+        </button>
+      );
+    }
+    return (
+      <button className="cell" key={idx}>
+        {idx}
+      </button>
+    );
+  };
 
   return (
     <div className="App">
@@ -26,18 +47,9 @@ function App() {
             .join(" "),
         }}
       >
-        {cells &&
-          cells.map((i) => (
-            <div
-              className={i < (width * width) / 2 ? "cell-active" : "cell"}
-              key={i}
-              onClick={() => handleClick(i)}
-            >
-              {i}
-            </div>
-          ))}
+        {cells && cells.map(Cell)}
       </div>
-      <pre>{JSON.stringify(clickedCells, 0, 2)}</pre>
+      <pre>{JSON.stringify(clickedCells)}</pre>
     </div>
   );
 }
